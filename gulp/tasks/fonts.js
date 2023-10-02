b089df067de63624dee52fcb3485da44f108ec99
magl88 <magl88net@gmail.com>
2023-10-02 14:30:53 +0200

    Update fonts.js

--- gulp/tasks/fonts.js
+++ gulp/tasks/fonts.js
@@ -1,8 +1,15 @@
+import pjson from '../../package.json' assert { type: 'json' };
 import newer from 'gulp-newer';
 
 export const fonts = () => {
   return app.gulp
     .src(app.path.src.assetsFolder + 'fonts/**/*')
     .pipe(newer(app.path.buildFolder + 'assets/fonts'))
+    .pipe(
+      replace(
+        /@fonts\//g,
+        pjson.buildType == 'wordpress' ? get_template_directory_uri() + 'assets/fonts/' : '/assets/fonts/'
+      )
+    )
     .pipe(app.gulp.dest(app.path.buildFolder + 'assets/fonts/'));
 };
