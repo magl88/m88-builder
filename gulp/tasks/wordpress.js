6938022a27ac058f6d9e252c2aea560a1aa31fb2
magl88 <magl88net@gmail.com>
2023-10-03 12:29:36 +0200

    update task for wordpress

--- gulp/tasks/wordpress.js
+++ gulp/tasks/wordpress.js
@@ -1,38 +1,42 @@
 import pjson from '../../package.json' assert { type: 'json' };
 import replace from 'gulp-replace';
+import pictureHtml from 'gulp-webp-avif-html-nosvg-nogif-lazyload';
 import gulp from 'gulp';
 
 export const wpFiles = () =>
   app.gulp
     .src([
       app.path.srcFolder + '**/*.php',
-      '!' + app.path.srcFolder + 'wordpress/**',
+      '!' + app.path.srcFolder + 'inc/**',
       '!' + app.path.srcFolder + 'components/**',
       '!' + app.path.srcFolder + 'php/**',
     ])
-    .pipe(replace(/@root\//g, get_template_directory_uri() + '/'))
-    .pipe(replace(/@src\//g, get_template_directory_uri() + '/'))
-    .pipe(replace(/@assets\//g, get_template_directory_uri() + '/assets/'))
-    .pipe(replace(/@fonts\//g, get_template_directory_uri() + '/assets/fonts/'))
-    .pipe(replace(/@img\//g, get_template_directory_uri() + '/assets/img/'))
-    .pipe(replace(/@sprite\//g, get_template_directory_uri() + '/assets/sprite/'))
-    .pipe(replace(/@svg\//g, get_template_directory_uri() + '/assets/svg/'))
-    .pipe(replace(/@scss\//g, get_template_directory_uri() + '/scss/'))
-    .pipe(replace(/@css\//g, get_template_directory_uri() + '/css/'))
-    .pipe(replace(/@js\//g, get_template_directory_uri() + '/js/'))
     .pipe(
       pictureHtml({
         primaryFormat: 'avif',
-        primaryAfter: '/assets/img/',
-        primaryBefore: '/assets/img/',
+        primaryAfter: '@img/',
+        primaryBefore: '@img/',
         secondaryFormat: 'webp',
-        secondaryAfter: '/assets/img/',
-        secondaryBefore: '/assets/img/',
+        secondaryAfter: '@img/',
+        secondaryBefore: '@img/',
         srcsetOutput: 0,
         youtubeCoverWebp: true,
       })
     )
+    .pipe(replace(/@root\//g, '<?php echo get_template_directory_uri();?>/'))
+    .pipe(replace(/@src\//g, '<?php echo get_template_directory_uri();?>/'))
+    .pipe(replace(/@assets\//g, '<?php echo get_template_directory_uri();?>/assets/'))
+    .pipe(replace(/@fonts\//g, '<?php echo get_template_directory_uri();?>/assets/fonts/'))
+    .pipe(replace(/@img\//g, '<?php echo get_template_directory_uri();?>/assets/img/'))
+    .pipe(replace(/@sprite\//g, '<?php echo get_template_directory_uri();?>/assets/sprite/'))
+    .pipe(replace(/@svg\//g, '<?php echo get_template_directory_uri();?>/assets/svg/'))
+    .pipe(replace(/@scss\//g, '<?php echo get_template_directory_uri();?>/scss/'))
+    .pipe(replace(/@css\//g, '<?php echo get_template_directory_uri();?>/css/'))
+    .pipe(replace(/@js\//g, '<?php echo get_template_directory_uri();?>/js/'))
     .pipe(app.gulp.dest(app.path.buildFolder));
+
+export const wpInc = () =>
+  app.gulp.src([app.path.srcFolder + 'inc/**/*']).pipe(app.gulp.dest(app.path.buildFolder + 'inc'));
 
 export const wpScreenshot = () =>
   app.gulp.src([app.path.srcFolder + 'screenshot.png']).pipe(app.gulp.dest(app.path.buildFolder));
@@ -43,4 +47,4 @@ export const wpStyle = () =>
     .pipe(replace('ThemeName', pjson.name))
     .pipe(app.gulp.dest(app.path.buildFolder));
 
-export const wordpress = gulp.parallel(wpStyle, wpFiles, wpScreenshot);
+export const wordpress = gulp.parallel(wpStyle, wpFiles, wpInc, wpScreenshot);
