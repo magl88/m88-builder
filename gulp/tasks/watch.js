9c79bd69111eac40a01ae44268175863d2c88fd3
magl88 <magl88net@gmail.com>
2023-10-13 09:46:29 +0200

    update run webServer reload

--- gulp/tasks/watch.js
+++ gulp/tasks/watch.js
@@ -1,20 +1,36 @@
-import pjson from '../../package.json' assert { type: 'json' };
+import projectConst from '../../package.json' assert { type: 'json' };
 import * as tasks from './index.js';
 
 export const watch = () => {
-  app.gulp.watch(app.path.buildFolder + '**/*').on('all', tasks.webServerCreate.reload);
-
-  app.gulp.watch(app.path.src.assetsFolder + 'svg/**/*').on('all', app.gulp.series(tasks.svg));
-  app.gulp.watch(app.path.src.assetsFolder + 'fonts/**/*').on('all', app.gulp.series(tasks.fonts));
-  app.gulp.watch(app.path.src.assetsFolder + 'favicons/**/*').on('all', app.gulp.series(tasks.favicons));
-  // app.gulp.watch(app.path.buildFolder + '**/*.{html,php}').on('all', app.gulp.series(tasks.setImg));
-  app.gulp.watch(app.path.srcFolder + '**/*.html').on('all', app.gulp.series(tasks.html));
+  app.gulp
+    .watch(app.path.src.assetsFolder + 'svg/**/*')
+    .on('all', app.gulp.series(tasks.svg, tasks.webServerCreate.reload));
+  app.gulp
+    .watch(app.path.src.assetsFolder + 'fonts/**/*')
+    .on('all', app.gulp.series(tasks.fonts, tasks.webServerCreate.reload));
+  app.gulp
+    .watch(app.path.src.assetsFolder + 'favicons/**/*')
+    .on('all', app.gulp.series(tasks.favicons, tasks.webServerCreate.reload));
+  app.gulp.watch(app.path.srcFolder + '**/*.html').on('all', app.gulp.series(tasks.html, tasks.webServerCreate.reload));
   app.gulp
     .watch(app.path.srcFolder + '**/*.php')
-    .on('all', app.gulp.series(pjson.buildType == 'php' ? tasks.php : tasks.wordpress));
-  app.gulp.watch(app.path.src.scssFolder + '**/*.*').on('all', app.gulp.series(tasks.scss));
-  app.gulp.watch(app.path.src.jsFolder + 'scripts/*.js').on('all', app.gulp.series(tasks.concatScripts));
-  app.gulp.watch(app.path.src.jsFolder + 'libs/*.js').on('all', app.gulp.series(tasks.concatLibs));
-  app.gulp.watch(app.path.src.jsFolder + '*.js').on('all', app.gulp.series(tasks.jsBuild));
-  app.gulp.watch(app.path.src.imagesFolder + '**/*.*').on('all', app.gulp.series(tasks.images));
+    .on(
+      'all',
+      app.gulp.series(projectConst.buildType == 'php' ? tasks.php : tasks.wordpress, tasks.webServerCreate.reload)
+    );
+  app.gulp
+    .watch(app.path.src.scssFolder + '**/*.*')
+    .on('all', app.gulp.series(tasks.scss, tasks.webServerCreate.reload));
+  app.gulp
+    .watch(app.path.src.jsFolder + 'scripts/*.js')
+    .on('all', app.gulp.series(tasks.concatScripts, tasks.webServerCreate.reload));
+  app.gulp
+    .watch(app.path.src.jsFolder + 'libs/*.js')
+    .on('all', app.gulp.series(tasks.concatLibs, tasks.webServerCreate.reload));
+  app.gulp
+    .watch(app.path.src.jsFolder + '*.js')
+    .on('all', app.gulp.series(tasks.jsBuild, tasks.webServerCreate.reload));
+  app.gulp
+    .watch(app.path.src.imagesFolder + '**/*.*')
+    .on('all', app.gulp.series(tasks.images, tasks.webServerCreate.reload));
 };
