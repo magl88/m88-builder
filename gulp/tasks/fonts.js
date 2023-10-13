3b9eb1b38ccd0e0635adf7a024da175ae114c536
magl88 <magl88net@gmail.com>
2023-10-13 09:42:33 +0200

    delete wrong replace

--- gulp/tasks/fonts.js
+++ gulp/tasks/fonts.js
@@ -1,16 +1,8 @@
-import pjson from '../../package.json' assert { type: 'json' };
 import newer from 'gulp-newer';
-import replace from 'gulp-replace';
 
 export const fonts = () => {
   return app.gulp
     .src(app.path.src.assetsFolder + 'fonts/**/*')
-    .pipe(newer(app.path.buildFolder + 'assets/fonts'))
-    .pipe(
-      replace(
-        /@fonts\//g,
-        pjson.buildType == 'wordpress' ? get_template_directory_uri() + 'assets/fonts/' : '/assets/fonts/'
-      )
-    )
+    .pipe(newer(app.path.buildFolder + 'assets/fonts/'))
     .pipe(app.gulp.dest(app.path.buildFolder + 'assets/fonts/'));
 };
