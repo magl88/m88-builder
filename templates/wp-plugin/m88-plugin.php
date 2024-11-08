cef63803e732b29ffd0ebf31a9ecc005989f443b
Eugene Rudoi <magl88net@gmail.com>
2024-11-08 10:44:28 +0100

    demo wp-plugin

---
+++ templates/wp-plugin/m88-plugin.php
@@ -0,0 +1,10 @@
+<?php
+/*
+Plugin Name: M88 Plugin
+Plugin URI: https://magl88.net
+Version: 1.0
+Author: magl88
+Author URI: https://magl88.net
+License: GPLv2 or later
+Text Domain: m88-plugin
+*/
